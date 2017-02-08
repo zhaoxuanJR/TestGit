d6f0dd200085132bcfed47cbede62d17d4de7e8d
walter zhao <18829344616@163.com>
2017-02-08 10:51:19 +0800

    2017/2/8windows
    windows add a function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void windows()
+{
+	cout<<"This is windows Os"<<endl;
+}
+
 int main()
 {
     cout << "Hello, World!" <<endl;
+    windows();
     return 0;
 }
